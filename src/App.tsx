60b2008f2978ce40ab3d3a20852ca244acc16d84
Poper84 <107497764+Poper84@users.noreply.github.com>
2024-03-04 20:05:37 -0700

    Linking the database!

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import collegeBasketballTeams from "./CollegeBasketballTeams.json";
+interface Team {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+function Welcome() {
+  return <h1>NCAA Basketball March Madness!</h1>;
+}
+
+class Band extends React.Component<Team> {
+  render() {
+    const oneBand = this.props;
+
+    return (
+      <div>
+        <h2>School Name: {oneBand.school}</h2>
+        <h2>Mascot: {oneBand.name}</h2>
+        <h2>City: {oneBand.city}</h2>
+        <h2>State: {oneBand.state}</h2>
+      </div>
+    );
+  }
+}
+function Bandlist() {
+  return (
+    <div>
+      {collegeBasketballTeams.teams.map((team: Team) => (
+        <Band key={team.school} {...team} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <Bandlist />
     </div>
   );
 }
